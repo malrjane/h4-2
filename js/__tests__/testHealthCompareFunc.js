043488204eb0f9d4cef76cb573c17de1569a8dde
malrjane <yana.ml02@mail.ru>
2024-03-20 22:37:44 +0400

    test updated

--- js/__tests__/testHealthCompareFunc.js
+++ js/__tests__/testHealthCompareFunc.js
@@ -1,6 +1,4 @@
 import healthCompare from "../logic";
-let expected;
-
 test("should compare characters' health from high to low", () => {
   const input = [
     { name: 'мечник', health: 10 },
@@ -8,15 +6,13 @@ test("should compare characters' health from high to low", () => {
     { name: 'лучник', health: 80 },
   ];
   const received = healthCompare(input, true);
-  for (let i = 0; i < input.length; i++) {
-    if (input[0].health > input[1].health && input[0].health > input[2].health) {
-      expected = [
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-        { name: 'мечник', health: 10 },        
-      ];
-    }
-  }
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
 
   expect(received).toEqual(expected);
 });
